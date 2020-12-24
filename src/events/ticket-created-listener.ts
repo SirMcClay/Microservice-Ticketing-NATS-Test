fb67dcc864e2bd7082dd53c95ff993a64951024d
sirmcclay <mcclay.design@gmail.com>
2020-12-24 14:12:28 -0300

    Readonly property to avoid annotate a immutable propertie

--- src/events/ticket-created-listener.ts
+++ src/events/ticket-created-listener.ts
@@ -4,7 +4,7 @@ import { TicketCreatedEvent } from './ticket-created-events';
 import { Subjects } from './subjects';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-	subject: Subjects.TicketCreated = Subjects.TicketCreated;
+	readonly subject = Subjects.TicketCreated;
 	queueGroupName = 'payments-service';
 
 	onMessage(data: any, msg: Message) {
